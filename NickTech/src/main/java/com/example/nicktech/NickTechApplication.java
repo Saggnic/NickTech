9068221b78c50bb091a033fbc3699f4bdd78a437
Saggnic <saggnic1@gmail.com>
2020-04-25 10:08:55 +0530

    dummyPush

--- NickTech/src/main/java/com/example/nicktech/NickTechApplication.java
+++ NickTech/src/main/java/com/example/nicktech/NickTechApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NickTechApplication {
-
+//
 	public static void main(String[] args) {
 		SpringApplication.run(NickTechApplication.class, args);
 	}
