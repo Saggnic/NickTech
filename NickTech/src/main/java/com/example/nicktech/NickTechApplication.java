34f355d4c27c3e4640b9546fab13abf0885a3845
Saggnic <saggnic1@gmail.com>
2020-04-25 10:05:01 +0530

    InitialCommit

---
+++ NickTech/src/main/java/com/example/nicktech/NickTechApplication.java
@@ -0,0 +1,13 @@
+package com.example.nicktech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NickTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NickTechApplication.class, args);
+	}
+
+}
---
+++ NickTech/src/test/java/com/example/nicktech/NickTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nicktech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NickTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
